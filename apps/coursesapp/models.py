53046765f5823b8e53c170266dfcad146ea2eed3
Felisa Deang <felisadee@gmail.com>
2016-07-19 14:35:40 -0700

    second commit. named routes. also added screenshots to show what the client can see

--- apps/coursesapp/models.py
+++ apps/coursesapp/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
 class Course(models.Model):
     course_name = models.CharField(max_length=150)
--- apps/coursesapp/urls.py
+++ apps/coursesapp/urls.py
@@ -2,9 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^addcourse$', views.addcourse),
-    url(r'^removecourse/(?P<id>\d+)$', views.removecourse),
-    url(r'^removethis/(?P<id>\d+)$', views.removethis),
-    url(r'^index$', views.index)
+    url(r'^$', views.index, name="index"),
+    url(r'^addcourse$', views.addcourse, name="addcourse"),
+    url(r'^removecourse/(?P<id>\d+)$', views.removecourse, name="removecourse"),
+    url(r'^removethis/(?P<id>\d+)$', views.removethis, name="removethis")
 ]
